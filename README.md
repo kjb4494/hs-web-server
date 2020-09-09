893af3f9cadf8214e6b433e34a9943cd9227bf2e
kjb4494 <rlagnltn1357@gmail.com>
2020-09-09 16:40:04 +0900

    directory structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hs-web-server
 Django API Server R&amp;D with React
+- [*Django project document*](https://docs.djangoproject.com/ko/3.1/topics/)
---
+++ secret_key_generator.py
@@ -0,0 +1,10 @@
+import string
+import random
+
+if __name__ == '__main__':
+    # Get ascii Characters numbers and punctuation (minus quote characters as they could terminate string).
+    chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
+
+    SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
+
+    print(SECRET_KEY)
